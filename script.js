3968ad8906247c2fa43dd17613dbccb7f083ee71
Rayy-007 <minkhant88887@gmail.com>
2023-09-05 17:10:22 +0630

    Created QR Code Completely

--- script.js
+++ script.js
@@ -2,13 +2,24 @@
 
 const qrEl = document.getElementById("qrBox");
 const qrImg = document.getElementById("qrImg");
-const qrText = document.getElementById("qrText");
+const qrInput = document.getElementById("qrInput");
 const btn = document.getElementById("btn");
+const qrText = document.getElementById("qrText");
 
 const generateQR = () => {
-  qrImg.src = `
-    https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrText.value}
+  if (qrInput.value) {
+    qrImg.src = `
+    https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrInput.value}
     `;
+    qrEl.classList.add("show-img");
+    qrText.innerText = qrInput.value;
+    qrInput.value = "";
+  } else {
+    qrInput.classList.add("error");
+    setTimeout(() => {
+      qrInput.classList.remove("error");
+    }, 2000);
+  }
 };
 
 btn.addEventListener("click", generateQR);
